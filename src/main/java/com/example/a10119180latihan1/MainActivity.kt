d8718794266b1ae3feba0b6fb0caf6d51bec1663
GheaRizqiN <104035815+GheaRizqiN@users.noreply.github.com>
2022-04-20 04:29:20 +0700

    Add files via upload

---
+++ src/main/java/com/example/a10119180latihan1/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.a10119180latihan1
+//tanggal pengerjaan : 19 April 2022
+//nim                : 10119180
+//nama               : Ghea Rizqi Nabila
+//kelas              : IF-5
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
